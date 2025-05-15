bc9366138443dfad0f12fe33b9480f1ac17e172c
matyashalla@gmail.com <matyashalla@gmail.com>
2025-05-15 18:29:44 +0200

    Importing path update

--- test_simple_math.py
+++ test_simple_math.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import pytest
-from Lesson_1.simple_math import SimpleMath
+from simple_math import SimpleMath
 
 @pytest.fixture
 def simple_math():
